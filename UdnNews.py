8251de99dcdc1236c60c33bdeb1aa152c6021102
unknown <chung30130@gmail.com>
2021-01-08 17:05:05 +0800

    .

--- UdnNews.py
+++ UdnNews.py
@@ -8,15 +8,27 @@ url="https://udn.com/news/breaknews/1"
 root=get.html.GetHtml(url)
 # bknews=root.find("section",class_='cate-list_subheader')
 
+#印出幻燈片
+# top=root.find("section",class_="slide-holder")
+# slide=top.find("div",class_="slide-section slide picture")
+# print(slide)
+
 #印出新聞分類
 # cat=root.find("header",class_="header")
 # nav=cat.find("nav",class_="navigation")
 # print(nav.text)
 
+#新聞子分類
+# subcat=root.find("section",class_="cate-list__subheader")
+# subcat_mode=subcat.find("a",class_="active")
+# print(subcat_mode.text)
+
 #印出即時新聞標題
 left=root.find("section",class_="thumb-news cate-list context-box")
 news=left.find("div",class_="context-box__content story-list__holder story-list__holder--full")
 news_text=news.find_all("div",class_="story-list__text")
-# print(headline)
+# print(news_text)
 for head in news_text:
     print(head.h2.text)
+
+
